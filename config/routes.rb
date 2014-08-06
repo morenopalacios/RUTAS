94797e7535b16cc02443e81a412ddaacc56f3b28
ingry moreno <ingri_17@hotmail.com>
2014-08-06 14:15:41 -0500

    modificacion de route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
 
   resources :proyects
 
+  resources :proyects do
+    resources :engineers
+end
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
